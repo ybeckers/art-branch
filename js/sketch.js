6c28143be6e823e22b04797e9e9bebf2d4147620
ybeckers <71328741+ybeckers@users.noreply.github.com>
2020-09-15 15:42:52 +0200

    Uploaded files

---
+++ js/sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(200, 200);
+    background(100);
+    print("dit is de setup in de print functie");
+
+    ellipseMode(CORNER);
+    ellipse(100,100,200,200);
+
+    rect(100,100,20,20);
+
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text("dit is het midden", 20, 70);
+}
+
+
+function draw(){
+
+
+
+}
